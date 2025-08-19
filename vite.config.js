f573cb53511a2a01c6aa284dc863bafbdd9f3d43
NadhiyaNashath <nashathnadhiya@gmail.com>
2025-08-19 20:19:30 +0530

    fix: correct base paths for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,16 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/algorithm-visualizer/',
+  base: '/algorithm-visualizer/',  
   plugins: [
     react(),
     tailwindcss(),
   ],
-  base: "/algorithm-visualizer/",
   server: {
-    opne: true,
+    open: true, 
     port: 3003
   }
 })
